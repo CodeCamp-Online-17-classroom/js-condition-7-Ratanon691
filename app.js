ab7271164f6d215fee465a667fa556320b8dd32e
Non T <117605943+Ratanon691@users.noreply.github.com>
2024-06-22 13:39:08 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
-// write code here
+let num1 = prompt(`give me a number`)
+let num2 = prompt(`give me a second number`)
+let value1 = +num1
+let value2 = +num2
+if (isNaN(num1) || isNaN(num2)){
+  alert(`Invalid number`)
+}
+else{
+  alert(`${value1} + ${value2} = ${value1 + value2}`)
+}
